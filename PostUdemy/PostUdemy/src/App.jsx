e70f35fae940597497f6911504a7e956500decf2
Sunil Karki <sunil.karki@spiralogics.com>
2023-06-18 20:26:10 +0300

    Initial commit

---
+++ PostUdemy/PostUdemy/src/App.jsx
@@ -0,0 +1,14 @@
+import PostList from "./components/PostList";
+
+function App() {
+  const body = "This is body";
+  const author = "Sunil Karki";
+
+  return (
+    <div>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
